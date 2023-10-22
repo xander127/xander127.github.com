702a377c6e1713a8c08ab89040e724adf920aaac
xander127 <alex02.00.mv@gmail.com>
2023-10-22 17:32:07 -0500

    Proyecto Boletines Add

---
+++ boletines/javascript/ejm.js
@@ -0,0 +1,26 @@
+//console.log("Hola Mundo desde un archivo de js");
+
+//DOM
+
+//---querySelector---
+/*let tabla = document.querySelector(".table");
+console.log(tabla);*/
+
+//---querySelectorAll--
+/*let links = document.querySelectorAll("a");*/
+
+//---recorrer arreglos---
+/*links.forEach(function(link){
+    console.log(link);
+});*/
+
+//---eventos---
+/*let celdas = document.querySelectorAll("td");
+
+celdas.forEach(function(td) {
+    td.addEventListener('click', function(){
+        //console.log("Click");
+        console.log(this);
+    })
+});*/
+
---
+++ boletines/javascript/main.js
@@ -0,0 +1,35 @@
+//obtener los elementos de la clase .close
+let links = document.querySelectorAll(".close");
+
+//recorrerlos
+links.forEach(function(link) {
+
+//agregar un evento click a cada uno de ellos
+    link.addEventListener("click", function(ev) {
+        //evitar que el evento siga su comportamiento por defecto
+        ev.preventDefault();
+        //return false;
+
+        let content = document.querySelector('.content');
+        //quitar las clases de animacion
+        content.classList.remove("animate__animated");
+        content.classList.remove("animate__fadeInDown"); 
+        //agregar clases para animar la salida
+        content.classList.add("animate__animated");
+        content.classList.add("animate__fadeOutUp"); 
+        
+        //timers
+        setTimeout(function(){
+            location.href="/";
+        },1000);
+
+        return false;
+
+    });
+});
+
+//quitar y agregar clases a un elemento con javascript
+/*let iconos = document.querySelectorAll("i");
+iconos.forEach(function(icono){
+    icono.classList.remove("fa-star");//remove - add
+});*/
